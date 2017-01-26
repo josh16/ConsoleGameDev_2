e5b2c5f667cff2c6f3c1c284c0040d898a5865cb
Josh.R <Josh@Joshs-Macbook-Pro-2.local>
2017-01-26 10:23:14 -0500

    Networking Fixed
    -Fixed a bit of the networking latency, so its much smoother but not a
    100 smooth
    - Bullets now shoot on both Host and Client
    - Default player will be “red” while other player is the colour
    “yellow”.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,21 +16,19 @@ public class Movement : NetworkBehaviour
 	public AudioClip hit;
 	//public AudioClip BossHit;
 
-	//Bullet Variables
-	public GameObject bullet;
-	public Transform spawner;
+	//bulletPrefab Variables
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
 	public float delayTime = 1.0f;
 	private float counter = 0;
 
 
 	//Grenade Variables
-	public Transform grenadeSpawner;
-	public Rigidbody grenade;
+	public Transform grenadeSpawn;
 	public float grenadeSpeed;
 	private float GrenadeCounter = 1.0f;
 	public float delayGrenadeTime = 5.0f;
 	public float numOfGrenades;
-
 
 
 	//Josh's Script
@@ -51,20 +49,18 @@ public class Movement : NetworkBehaviour
 			return;
 		}
 
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdKeyboardInput();
+		}
+
+
 		//Firing Gun Function/ Keyboard and Controller input.
-		CmdGun();
-		CmdKeyboardInput ();
-
-
-        
-		CmdGrenade();
+		//CmdGun();
+		//CmdGrenade();
         //Fire ();
 		//Grenade ();
 
-
-
-
-	//Movement Code
+		//Movement Code
 		float hAxis = Input.GetAxis ("Horizontal");
 		float vAxis = Input.GetAxis ("Vertical");
 		 
@@ -83,27 +79,27 @@ public class Movement : NetworkBehaviour
 		//movement Code
 	
 
-
 	}
+
 
 	//On Start the material will be set to blue to identify which gameobject belongs to the player 
 	public override void OnStartLocalPlayer()
 	{
-		GetComponent<MeshRenderer>().material.color = Color.blue;
+		GetComponent<MeshRenderer>().material.color = Color.red;
 	}
 
-
+	/*
 	[Command] // Command here indicates that the following function will be called by the client,but will be run on the server.
-    void CmdGun()//When making a networked command, the function name must being with "Cmd"
+    void CmdGun() //When making a networked command, the function name must being with "Cmd"
     {
         //Gun Code
         if (Input.GetButtonDown("PS4_R1"))
         {
-            Instantiate(bullet, spawner.position, transform.rotation);
+			Instantiate(bulletPrefab, bulletSpawn, transform.rotation);
 
 			//Networking
-			NetworkServer.Spawn(bullet);//Spawn the bullet on the Clients
-			Destroy(bullet,2.0f);
+			NetworkServer.Spawn(bulletPrefab);//Spawn the bulletPrefab on the Clients
+			Destroy(bulletPrefab,2.0f);
 
 			AudioSource.PlayClipAtPoint(Shoot, transform.position);
             counter = 0;
@@ -119,7 +115,7 @@ public class Movement : NetworkBehaviour
         //Grenade code
         if (Input.GetButtonDown("PS4_L1"))
         {
-            Rigidbody instantiatedgrenade = Instantiate(grenade, grenadeSpawner.position, transform.rotation) as Rigidbody;
+			Rigidbody instantiatedgrenade = Instantiate(grenade, grenadeSpawn, transform.rotation) as Rigidbody;
             instantiatedgrenade.velocity = transform.TransformDirection(new Vector3(0, 0, grenadeSpeed));
 
             numOfGrenades--;
@@ -131,7 +127,7 @@ public class Movement : NetworkBehaviour
         delayGrenadeTime += Time.deltaTime;
     }
     
-
+	*/
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.CompareTag ("Enemy")) 
@@ -177,7 +173,7 @@ public class Movement : NetworkBehaviour
 		float triggerAxis = Input.GetAxis("PS4_RTrigger");
 
 		if (triggerAxis != 0 && counter > delayTime) {
-			Instantiate (bullet, spawner.position, transform.rotation);
+			Instantiate (bulletPrefab, spawbulletSpawnition, transform.rotation);
 			AudioSource.PlayClipAtPoint(Shoot,transform.position);
 			counter = 0;
 
@@ -200,7 +196,7 @@ public class Movement : NetworkBehaviour
 
 		if (triggerAxis != 0 && counter > delayGrenadeTime) 
 		{
-			Rigidbody instantiatedgrenade = Instantiate (grenade, grenadeSpawner.position, transform.rotation) as Rigidbody;
+			Rigidbody instantiatedgrenade = Instantiate (grenade, grenadeSpawbulletSpawnition, transform.rotation) as Rigidbody;
 			instantiatedgrenade.velocity = transform.TransformDirection (new Vector3 (0, 0, -grenadeSpeed));
 
 
@@ -222,19 +218,31 @@ public class Movement : NetworkBehaviour
 	[Command]
 	void CmdKeyboardInput()
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) 
-		{
-			Instantiate (bullet, spawner.position, transform.rotation);
-			//Spawn the bullet on the Clients
-			//Networking the bullet :)
+			//Instantiate (bulletPrefab, bulletSpawn, transform.rotation);
+			// Create the Bullet from the Bullet Prefab
+			var bullet = (GameObject)Instantiate(
+				bulletPrefab,
+				bulletSpawn.position,
+				bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+			bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
 			NetworkServer.Spawn(bullet);
-			Destroy (bullet, 2.0f);
+
+			// Destroy the bullet after 2 seconds
+			Destroy(bullet, 2.0f);        
+
+			//Spawn the bulletPrefab on the Clients
+			//Networking the bulletPrefab :)
+
+			//Destroy (bulletPrefab, 2.0f);
 			AudioSource.PlayClipAtPoint(Shoot,transform.position);
-			counter = 0;
+			//counter = 0;
 
 
-		}
-		counter += Time.deltaTime;
+		
+		//counter += Time.deltaTime;
 	}
 
 
