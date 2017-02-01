53f37bb0758eeb7db4039bf5a95c8a27381d386f
Josh.R <Josh@Joshs-Macbook-Pro-2.local>
2017-02-01 11:28:28 -0500

    New Health script Added
    -Added a new health script but its not attached to the player yet, i
    will be adding the code from that script to the new one soon
    -Added a bullet script and attached it to the bullet for destroying it
    and etc.
    -Deleted some old code which was commented out inside UI script.

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	//Health variables
+	public const int maxHealth = 100;
+
+	public int currentHealth = maxHealth;
+
+
+
+	public void TakeDamage(int amount)
+	{
+		currentHealth -= maxHealth;
+
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+
+			Debug.Log("Oh no! i'm Dead!!");
+			//Code right here will destroy the player
+		}
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+
+
+
+}
