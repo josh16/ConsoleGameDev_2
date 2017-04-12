f846e867b689fb6086e9423d1d63b03effbd7430
MatthewRadeff <matt_radeff@hotmail.com>
2017-04-12 12:00:02 -0400

    dino count
    dino count for zachs scene

---
+++ Assets/dinocount.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class dinocount : MonoBehaviour
+{
+
+    public int m_dinoNumber = 100;
+
+    public Text m_dinotext;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
